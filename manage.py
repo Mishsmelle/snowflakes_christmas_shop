d82c32b8eff1cadf9fa1047ee996a0e62703e0d7
Mishsmelle <michelle.oconnor@shopify.com>
2021-10-17 14:39:50 +0000

    added shopping bag app urls and templates

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snowflakes_christmas_shop.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'snowflakes_christmas_shop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- snowflakes_christmas_shop/settings.py
+++ snowflakes_christmas_shop/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
-    
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +67,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
