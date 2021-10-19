975cb034ad3f04a9d7ec63c61a30ec4beed6485e
Mishsmelle <michelle.oconnor@shopify.com>
2021-10-19 19:31:41 +0000

    updated shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -21,5 +20,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
