d4d7d378196f608555f4c1171c9cdfb3f3c85911
Mishsmelle <michelle.oconnor@shopify.com>
2021-10-20 20:34:08 +0000

    created check out app and models

--- snowflakes_christmas_shop/settings.py
+++ snowflakes_christmas_shop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boutique_ado.urls'
+ROOT_URLCONF = 'snowflakes_christmas_shop.urls'
 
 TEMPLATES = [
     {
@@ -98,7 +99,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado.wsgi.application'
+WSGI_APPLICATION = 'snowflakes_christmas_shop.wsgi.application'
 
 
 # Database
