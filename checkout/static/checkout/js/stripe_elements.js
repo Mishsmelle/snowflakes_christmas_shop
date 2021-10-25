5a9a3848bf4c81b60e003a7f72facc517a5a64db
Mishsmelle <michelle.oconnor@shopify.com>
2021-10-25 21:31:43 +0000

    fixed checkout error

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,11 +4,13 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
-
+//Get the stripe public key and client secret.  Slice off first and last character as don't need quote marks.
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
+//Set up stripe by creating variable using stripe public key.  Made possible by stripe js in base template.
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
+
 var style = {
     base: {
         color: '#000',
@@ -24,7 +26,10 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {style: style});
+
+var card = elements.create('card', {
+    style: style
+});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
@@ -32,8 +37,8 @@ card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
-            <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
+            <span role="alert">
+            <i class="bi bi-x-circle"></i>
             </span>
             <span>${event.error.message}</span>
         `;
@@ -47,26 +52,36 @@ card.addEventListener('change', function (event) {
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
+    //prevent the default action which is POST
     ev.preventDefault();
+    //disable cart element & submit button to prevent multiple submissions
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    //trigger overlay and fade out form 
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-
+    //get the Boolean value of the saved info box by looking at its check attribute
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
+    //From using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    //Object to pass info to the cache_checkout_data view
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
     };
     var url = '/checkout/cache_checkout_data/';
-
+    
+    //Post data to the cache_checkout_data view
+    //Wait for a response that payment intent was updated by adding 
+    //'done' method and executing the callback function
     $.post(url, postData).done(function () {
+        //confirm card payment method, provide card to stripe & execute function on result
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
+                //Add form data
+                //Post code not added as this will come from card element & stripe would overide
                 billing_details: {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
@@ -80,6 +95,7 @@ form.addEventListener('submit', function(ev) {
                     }
                 }
             },
+            //Add shipping info as might be different from billing
             shipping: {
                 name: $.trim(form.full_name.value),
                 phone: $.trim(form.phone_number.value),
@@ -93,24 +109,30 @@ form.addEventListener('submit', function(ev) {
                 }
             },
         }).then(function(result) {
+            //if there's an error put this into the card error div
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
                 var html = `
-                    <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
+                    <span role="alert">
+                    <i class="bi bi-x"></i>
                     </span>
                     <span>${result.error.message}</span>`;
+                    //if error re-enable the card element and submit button to allow
+                    //the user to fix it
                 $(errorDiv).html(html);
+                //hide the loading overlay
                 $('#payment-form').fadeToggle(100);
                 $('#loading-overlay').fadeToggle(100);
                 card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
+                //else if status of the payment intent is succeeded submit the form
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
                     form.submit();
                 }
             }
         });
+        //Failure function triggered if our view sends a 400 bad request response
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
