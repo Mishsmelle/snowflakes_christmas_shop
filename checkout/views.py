edbdffb3f8bbb4e42653a197023be4ea807af91a
Mishsmelle <michelle.oconnor@shopify.com>
2021-10-25 15:14:42 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jg76CFGdZvUD3TrvQUcV9YKun4GupoZeEGpBvCguQlPltihu4GprWqi7v8TXeFzFIhLZXZuVOBDHFxQVTbPwShE00k1kxyOcl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
