1a3d73b3107f794a3939da204822a76468b3ebb0
Mishsmelle <michelle.oconnor@shopify.com>
2021-11-14 20:06:20 +0000

    starting contact page functionlity

--- home/views.py
+++ home/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
-# Create your views here.
-""" A View to return the index page"""
 
+def home(request):
+    """ This view returns the index page"""
 
-def index(request): 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    '''This view returns contact page and
+       posts the contact form information to the db
+    '''
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid:
+            form.subject = request.POST['subject'],
+            form.message = request.POST['message'],
+            form.email = request.POST['email'],
+            form.save()
+            user_email = ''.join(form.email)
+            messages.success(request,
+                             f"Thanks { user_email }, Your message has been sent.\
+                             We will be in touch shortly.")
+            return redirect("home")
+        else:
+            messages.error(request,
+                           'Error: something has gone wrong \
+                            please try again later.')
+            return redirect('home')
+    else:
+        form = ContactForm()
+
+    template = 'home/contact_us.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request,
+                  template,
+                  context)
