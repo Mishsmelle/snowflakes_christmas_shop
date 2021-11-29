314b533267a48ade209fe3fad88cf32cc63b5516
Mishsmelle <michelle.oconnor@shopify.com>
2021-11-29 21:19:48 +0000

    added settings for sending real emails

--- snowflakes_christmas_shop/settings.py
+++ snowflakes_christmas_shop/settings.py
@@ -210,4 +210,15 @@ STRIPE_CURRENCY = 'eur'
 # STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51Jg76CFGdZvUD3TrvQUcV9YKun4GupoZeEGpBvCguQlPltihu4GprWqi7v8TXeFzFIhLZXZuVOBDHFxQVTbPwShE00k1kxyOcl')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51Jg76CFGdZvUD3TrTsV99PgL5rLAepqwlz5rmCcVEvyixcfIOCDtaAqvyNtpMsDTjiL1eqaOBNtCU62lKqRRAeKh00PuR9ZWx2')
-DEFAULT_FROM_EMAIL = 'info@snowflakeschristmasshop.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@snowflakeschristmasshop.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
