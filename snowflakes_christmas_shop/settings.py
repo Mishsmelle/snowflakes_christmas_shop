02a25e99e1ab252619cb2cd44fbd8a7c4389ccce
Mishsmelle <michelle.oconnor@shopify.com>
2021-11-29 22:14:54 +0000

    unhid email in development mode line from settings

--- snowflakes_christmas_shop/settings.py
+++ snowflakes_christmas_shop/settings.py
@@ -211,10 +211,10 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51Jg76CFGdZvUD3TrvQUcV9YKun4GupoZeEGpBvCguQlPltihu4GprWqi7v8TXeFzFIhLZXZuVOBDHFxQVTbPwShE00k1kxyOcl')
 # STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51Jg76CFGdZvUD3TrTsV99PgL5rLAepqwlz5rmCcVEvyixcfIOCDtaAqvyNtpMsDTjiL1eqaOBNtCU62lKqRRAeKh00PuR9ZWx2')
 
-#if 'DEVELOPMENT' in os.environ:
-#    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#    DEFAULT_FROM_EMAIL = 'info@snowflakeschristmasshop.com'
-#else:
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@snowflakeschristmasshop.com'
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
