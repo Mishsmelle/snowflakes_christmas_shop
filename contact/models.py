5578dc61028ff2d04bdfdadc8211b65c79efecd3
Mishsmelle <michelle.oconnor@shopify.com>
2021-11-30 20:58:41 +0000

    added contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Contact Model for customers to contact the admin and store owner
+    """
+
+    subject = (
+    ('general', 'General Questions'),
+    ('product_information', 'Product Information'),
+    ('order_status', 'Order Status'),
+    ('shipping_delivery', 'Shipping and Delivery'),
+    ('bulk_purchasing', 'Wholesale Purchasing'),)
+    
+    class Meta:
+        ordering = ("-message_date",)
+        
+        verbose_name_plural = 'Messages'
+
+    name = models.CharField(max_length=254)
+    email = models.EmailField()
+    subject = models.CharField(max_length=254, choices=subject,
+                               default='general')
+    message = models.TextField()
+    message_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
