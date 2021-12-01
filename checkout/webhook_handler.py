1edaa4f320d423060a3727337e30c9ddee50b1c4
Mishsmelle <michelle.oconnor@shopify.com>
2021-12-01 20:40:03 +0000

    fixed missing { from login.html that was preventing the page from loading correctly

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -8,10 +8,13 @@ from products.models import Product
 from profiles.models import UserProfile
 import json
 import time
+
+
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
     def __init__(self, request):
         self.request = request
+
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
@@ -21,13 +24,14 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
+
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )        
+        )
+
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
@@ -35,6 +39,7 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
+
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
@@ -137,6 +142,7 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
+
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
