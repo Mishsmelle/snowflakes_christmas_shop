d35eed5f15b952843a5d14390fc9b24d6bda3251
Mishsmelle <michelle.oconnor@shopify.com>
2021-12-13 20:14:57 +0000

    bag views.py remved parenthesis from import line

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import (
-    render, redirect, reverse, HttpResponse, get_object_or_404)
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -79,27 +78,16 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
-
+    """ Remove an item from the shopping bag """
     try:
         product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
         bag = request.session.get('bag', {})
-
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
-        else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
-
+        bag.pop(item_id)
+        messages.success(
+            request, f'Removed {product.title} from your bag')
         request.session['bag'] = bag
-        return HttpResponse(status=200)
 
+        return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
